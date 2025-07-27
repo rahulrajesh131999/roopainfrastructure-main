b0a3e4f0140d922e5dde15fc38b14e0939ca5753
rahul <rahulraajesh138@gmail.com>
2025-07-27 19:06:50 +0530

    metamodification

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-
-import './App.css'
-import Navbar from './components/common/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Error from './components/common/Error'
-import SEO from './components/SEO'
-
+import "./App.css";
+import Navbar from "./components/common/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./components/common/Error";
+import SEO from "./components/SEO";
 
 function App() {
-
   return (
-    <div className='w-screen min-h-screen flex flex-col bg-black text-white font-inter '>
-      <SEO title='Roopa Infrastructures' description='Welcome to Roopa Infrastructures, building the future. '/>
+    <div className="w-screen min-h-screen flex flex-col bg-black text-white font-inter ">
+      <SEO
+        title="Roopa Infrastructures"
+        description="At Roopa Infrastructures, we don’t just construct buildings—we
+                  craft strong foundations for communities, industries, and
+                  futures. "
+      />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='*' element={<Error/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
